070f7a8496c2cf4d0ba502d5ffe66d03f0b26319
TarakaKoda <taraka.koda@gmail.com>
2023-05-13 23:52:18 +0530

    comparison operators used in WHERE clause

---
+++ 02-The WHERE Clause/2.2 Comparison Operators.sql
@@ -0,0 +1,13 @@
+-- List of comparison operators
+-- >  (Greater Than)
+-- <  (Less Than)
+-- >= (Greater Than Equals To)
+-- <= (Less Than Equals To)
+-- =  (Equals To)
+-- != (Not Equals To)
+-- <> (Not Equals To)
+
+
+SELECT * 
+FROM customers
+WHERE state = "VA"
