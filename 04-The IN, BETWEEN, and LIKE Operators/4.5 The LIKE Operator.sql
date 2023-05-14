a8916e23b14c629c4972871e05776059b7eefd0e
TarakaKoda <taraka.koda@gmail.com>
2023-05-14 23:56:06 +0530

    LIKE Operator

---
+++ 04-The IN, BETWEEN, and LIKE Operators/4.5 The LIKE Operator.sql
@@ -0,0 +1,10 @@
+SELECT *
+FROM customers
+-- WHERE last_name LIKE "b%" 
+-- WHERE last_name LIKE "brush%"
+-- WHERE last_name LIKE "%b%"
+-- WHERE last_name LIKE "%y"
+-- WHERE last_name LIKE "_____y"
+WHERE last_name LIKE "b____y"
+-- % any number of characters
+-- _ single character
