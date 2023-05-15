7a645682d408920bed870ff5dbc299dea2283238
TarakaKoda <taraka.koda@gmail.com>
2023-05-15 23:45:48 +0530

    Sorting based on the column position

---
+++ 07-The ORDER BY Clause/7.5 Sorting The Data Using The Column Positions.sql
@@ -0,0 +1,8 @@
+SELECT first_name, last_name
+FROM customers
+ORDER BY 1,2
+
+-- 1,2 means the positon of the selected columns
+-- Here 1,2 means sort the data using first_naem, and last_name 
+
+-- Note: This type of sortig is not the best practice always use the column names for sorting 
