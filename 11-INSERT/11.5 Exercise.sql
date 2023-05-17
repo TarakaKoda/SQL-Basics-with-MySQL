025ca5edab9f82e8a9a95e0bc9392cb6d7f1d6de
TarakaKoda <taraka.koda@gmail.com>
2023-05-17 23:16:22 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 11-INSERT/11.5 Exercise.sql
@@ -0,0 +1,6 @@
+-- Insert three rows into a product table
+
+INSERT INTO products (name, quantity_in_stock, unit_price)
+VALUES ("Sample Product1", 7, 11.5),
+	   ("Sample Producct2", 9, 10.5),
+       ("Sample Product3", 8, 7.5)
