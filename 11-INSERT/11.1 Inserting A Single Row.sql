341dcbf5463eea5e039b66f052727a15be4a387c
TarakaKoda <taraka.koda@gmail.com>
2023-05-17 23:14:57 +0530

    inserting a single row using INSERT

---
+++ 11-INSERT/11.1 Inserting A Single Row.sql
@@ -0,0 +1,10 @@
+INSERT INTO customers (first_name, last_name, birth_date, address, state, city, points)
+VALUES (
+	"Taraka",
+    "Koda",
+    "2001-06-02",
+    "address3",
+    "LA",
+    "city3  ",
+    3000
+)
