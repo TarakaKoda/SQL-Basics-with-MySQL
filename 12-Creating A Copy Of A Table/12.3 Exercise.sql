e5a9634bc0daa7f6c983c21915ae52705a49cfc5
TarakaKoda <taraka.koda@gmail.com>
2023-05-18 23:43:36 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 12-Creating A Copy Of A Table/12.3 Exercise.sql
@@ -0,0 +1,13 @@
+CREATE TABLE invoices_archived
+SELECT
+	c.name,
+    i.invoice_id,
+    i.number,
+    invoice_total,
+    payment_total,
+    invoice_date,
+    due_date,
+    payment_date
+FROM invoices i
+JOIN clients c USING (client_id)
+WHERE i.payment_date IS NOT NULL
