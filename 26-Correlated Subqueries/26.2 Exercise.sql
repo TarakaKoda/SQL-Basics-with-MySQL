73a0a0365df768e975578d61d7ecbd789601d304
TarakaKoda <taraka.koda@gmail.com>
2023-05-20 18:17:53 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 26-Correlated Subqueries/26.2 Exercise.sql
@@ -0,0 +1,9 @@
+-- Get invoices that are larger than the client's average invoice amount
+SELECT *
+FROM invoices i
+WHERE i.invoice_total > (
+	SELECT AVG(invoice_total)
+    FROM invoices
+    WHERE client_id = i.client_id
+)
+
