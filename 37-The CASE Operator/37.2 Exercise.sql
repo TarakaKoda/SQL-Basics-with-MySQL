e15f3c42e2c4bc7665c6a774d9652cae719f8fdf
TarakaKoda <taraka.koda@gmail.com>
2023-05-21 17:03:41 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 37-The CASE Operator/37.2 Exercise.sql
@@ -0,0 +1,10 @@
+SELECT 
+	CONCAT(first_name, " ", last_name) AS customer,
+    points,
+    CASE 
+		WHEN points >= 3000 THEN "Gold"
+        WHEN points BETWEEN 2000 AND 3000 THEN "Silver"
+        ELSE "Bronze"
+	END AS category
+FROM customers
+ORDER BY points DESC
